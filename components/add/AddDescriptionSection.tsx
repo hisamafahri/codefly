a923361407f3706aa9c2e4598daebfc2b0adf6fd
Hisam Fahri <me@hisamafahri.com>
2022-05-27 15:32:57 +0700

    fix(build): fix svg error

--- components/add/AddDescriptionSection.tsx
+++ components/add/AddDescriptionSection.tsx
@@ -16,11 +16,11 @@ export default function AddDescriptionSection() {
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            stroke-width="2"
+            strokeWidth="2"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M17 8l4 4m0 0l-4 4m4-4H3"
             />
           </svg>
