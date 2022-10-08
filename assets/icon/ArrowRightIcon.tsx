fa8c97667d74cdbe315526754e7cf719732be4ae
Hisam Fahri <me@hisamafahri.com>
2022-10-08 20:38:46 +0700

    feat(main): add description section

---
+++ assets/icon/ArrowRightIcon.tsx
@@ -0,0 +1,12 @@
+import { SVGProps } from "react";
+
+export function ArrowRightIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="m14 18l-1.4-1.45L16.15 13H4v-2h12.15L12.6 7.45L14 6l6 6Z"
+      ></path>
+    </svg>
+  );
+}
---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            name="description"
+            content="CodeFly - Share your codes easily!"
+          />
+          <link rel="icon" href="https://fav.farm/🚀" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="font-inter">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
