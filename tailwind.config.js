9103f3390f2b953e46f6ffff56ce744e9cdf3bb2
Hisam Fahri <me@hisamafahri.com>
2022-10-08 19:39:34 +0700

    feat(global): clean start for v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        "base-200": "#1E1E1E",
-        "primary-light": "#8849f4",
-      },
-    },
+    extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [],
 };
